50a8b5547a9f6f1df82cc1ed55888403de556b67
Gento Kato <gento.badger@gmail.com>
2019-03-18 13:47:12 -0700

    Ver. 0.0.0.001

--- R/read_scrapeBill.R
+++ R/read_scrapeBill.R
@@ -8,51 +8,94 @@
 #' @param file A connection or the name of the file (use \code{.rds} extension).
 #' @param ... Additional argument passed to \code{\link[base]{saveRDS}}
 #' 
-#' @return For \code{read_scrapeBill}, an \code{R} object. For \code{write_scrapeBill}, \code{NULL} invisibly.
+#' @return For \code{read_scrapeBill}, an \code{R} object. 
+#' For \code{write_scrapeBill}, \code{NULL} invisibly. \code{write_scrapeBill} also writes out 
+#' auxiliary files under \code{FILENAME_files} folder. DON'T delete the folder. 
+#' \code{read_scrapeBill} will need the files in the folder to read file.
 #' 
-#' @importFrom xml2 read_html
+#' @importFrom xml2 xml_serialize
+#' @importFrom xml2 xml_unserialize
+#' @importFrom tools file_path_sans_ext
+#' @importFrom pbapply pblapply
+#' @importFrom rvest html_nodes
 #' 
 #' @export
-read_scrapeBill = function(file) {
-
-  #load from file
-  x = readRDS(file)
+read_scrapeBill <- function(file) {
   
-  readF <- function(k) {
-    k$info <- read_html(k$info)
-    k$text <- read_html(k$text)
+  filename <- basename(tools::file_path_sans_ext(file))
+  foldername <- paste0(filename,"_files")
+  dirloc <- paste(dirname(file),foldername,sep="/")
+  if (dir.exists(dirloc)==FALSE) stop("auxiliary files not found!")
+  
+  # Read File
+  x <- readRDS(file)
+  
+  # Put xml object back
+  xmlback <- function(obj) {
+    tf <- paste(dirloc,obj,sep="/")
+    con <- file(tf, "rb")
+    on.exit(close(con), add = TRUE)
+    res <- xml_unserialize(con)
+    return(res)
+  }
+  
+  writexml <- function(k) {
+    k$info <- xmlback(k$info)
+    k$text <- xmlback(k$text)
+    class(k) <- "scrapeBill"
     return(k)
   }
   
-  # Read
   if (class(x)[1]=="scrapeBill") {
-    x <- readF(x)
-    class(x) <- "scrapeBill"
+    x <- writexml(x)
   } else if (class(x)[1]=="list" & class(x[[1]])=="scrapeBill") {
-    x <- lapply(x, readF)
-    for (i in 1:length(x)) class(x[[i]]) <- "scrapeBill"
+    x <- pblapply(x, function(k) writexml(k))
   } else {
     stop("invalid object type.")
   }
   
   return(x)
+  
 }
 
 #' @rdname read_scrapeBill
 #' @export
-write_scrapeBill = function(x, file, ...) {
+write_scrapeBill <- function(x, file, ...) {
   
-  #convert to character
+  filename <- basename(tools::file_path_sans_ext(file))
+  foldername <- paste0(filename,"_files")
+  dirloc <- paste(dirname(file),foldername,sep="/")
+  if (dir.exists(dirloc)==TRUE) {
+    unlink(paste0(dirloc,"/*"))
+  } else {
+    dir.create(dirloc)
+  }
+
+  locback <- function(obj) {
+    obj <- html_nodes(obj, xpath="/html")
+    tf <- tempfile(tmpdir=dirloc)
+    con <- file(tf, "wb")
+    xml_serialize(obj, con)
+    on.exit(close(con), add = TRUE)
+    return(basename(tf))
+  }
+  
+  writeloc <- function(k) {
+    k$info <- locback(k$info)
+    k$text <- locback(k$text)
+    class(k) <- "scrapeBill"
+    return(k)
+  }
+  
   if (class(x)[1]=="scrapeBill") {
-    x <- lapply(x, as.character)
-    class(x) <- "scrapeBill"
+    x <- writeloc(x)
   } else if (class(x)[1]=="list" & class(x[[1]])=="scrapeBill") {
-    x <- lapply(x, function(k) lapply(k, as.character))
-    for (i in 1:length(x)) class(x[[i]]) <- "scrapeBill"
+    x <- pblapply(x, function(k) writeloc(k))
   } else {
     stop("invalid object type.")
   }
-
+  
   #save
   saveRDS(x, file = file, ...)
 }
+
